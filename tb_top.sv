fbe6fd6aa3ef00ee7c221dcf6a3b1bce3c9e776c
kirushnamurthy <kirushnamurthy2410@gmail.com>
2026-02-04 12:04:09 +0530

    added clk and reset

--- tb_top.sv
+++ tb_top.sv
@@ -3,4 +3,29 @@ module tb_top();
         $display("Hello, SystemVerilog!");
         $finish;
     end
+
+    initial begin
+        fork
+            // Call the reset generation task
+            reset_generation(); 
+        join_none
+        // Call the clock generation task
+        clock_generation();
+        
+    end
+
+    task reset_generation();
+        $display("Reset generation started.");
+        #5ns;
+        $display("Reset de-asserted after 5ns.");
+    endtask
+
+    task clock_generation();
+        $display("Clock generation started.");
+        forever begin
+            #2ns;
+            $display("Clock tick at time %0t", $time);
+        end 
+    endtask
+    
 endmodule 
